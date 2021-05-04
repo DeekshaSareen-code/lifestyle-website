cf763e4b9085ca394e36d5cc4ec9f273fc5c4de8
Deeksha Sareen <83628412+DeekshaSareen-code@users.noreply.github.com>
2021-05-04 19:45:19 +0545

    Update check_if_added.php

--- includes/check_if_added.php
+++ includes/check_if_added.php
@@ -3,7 +3,7 @@ function check_if_added_to_cart($items_id)
 {
     require 'includes/common.php';
     
-    $con = mysqli_connect("localhost:3308", "root", "", "ebudget") or die(mysqli_error($con));
+    $con = mysqli_connect("localhost:3308", "root", "", "ecommerce") or die(mysqli_error($con));
     $user_id = $_SESSION['id'] ;
     $sqlquery= "SELECT * FROM users_items WHERE items_id='$items_id' AND user_id ='$user_id' and status='Added to cart'";
     $result = mysqli_query($con, $sqlquery) or die(mysqli_error($con));
